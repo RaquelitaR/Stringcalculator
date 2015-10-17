4db7c990b51c5986af8e492bff78caeea09f5b59
Raquelita15 <raquelita15@ru.is>
2015-10-17 12:48:27 +0000

    Test 4 - Testing multiple numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -9,24 +9,29 @@ public class CalculatorTest {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
-    // Test 1
+    // Test 1 - Is this an empty string
     @Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
 
-	// Test 2
+	// Test 2 - Check if string contains one number
 	@Test 
 	public void testOneNumber(){
 		assertEquals(1, Calculator.add("1"));
 	}
 
-	// Test 3
+	// Test 3 - Sum of two numbers
 	@Test
 	public void testTwoNumbers(){
 		assertEquals(3, Calculator.add("1,2"));
 	}
 
+	// Test 4 - Sum of multiple numbers
+	@Test
+	public void testMultNumbers(){
+		assertEquals(15, Calculator.add("1,2,3,4,5"));
+	}
 
 
 }
